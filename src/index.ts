a6a20cb9d10221dcd58f09aa88b20f31f9c2cb85
Piroska Werner <piroska.werner@gmail.com>
2021-07-12 12:28:50 -0500

    feat: Adds express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Sun is shining, birds are chirping.');
+import express from 'express'
+
+const app = express();
+const port = 3000; //can be any number > 1024
+
+// define route handler for the default home page
+app.get('/api', (req, res) => {
+    res.send('server is working');
+});
+
+// start the Express server
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+});
