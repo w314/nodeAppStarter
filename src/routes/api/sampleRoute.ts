e01ef192e2b28e45db320dc0f2b873e90b97d94f
Piroska Werner <piroska.werner@gmail.com>
2021-07-25 18:37:03 -0500

    feat: Adds custom made logger middleware

--- src/routes/api/sampleRoute.ts
+++ src/routes/api/sampleRoute.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import logger from '../utilities/logger';
 
 //create router object
 const sampleRoute = express.Router();
 
 //set up route
-sampleRoute.get('/', (req, res) => {
+sampleRoute.get('/', logger, (req, res) => {
   res.send('Application starting page');
 });
 
