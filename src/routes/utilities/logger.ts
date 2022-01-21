d53ee5304cda40c3af4415dd263e56f6cd8df243
Piroska Werner <piroska.werner@gmail.com>
2022-01-20 20:08:38 -0600

    fix: Change type of next in logger middleware

--- src/routes/utilities/logger.ts
+++ src/routes/utilities/logger.ts
@@ -1,9 +1,14 @@
-import express from 'express'
+import express from 'express';
 
-const logger = (req: express.Request, res: express.Response, next: Function): void => {
-	const url = req.baseUrl;
-	console.log(`${url} was visited`);
-	next();
-}
+// middleware that logs the endpoint to the console when visited
+const logger = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+): void => {
+  const url = req.baseUrl;
+  console.log(`${url} was visited`);
+  next();
+};
 
-export default logger
+export default logger;
